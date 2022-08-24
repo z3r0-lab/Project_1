6644c74febbb749f9cbf30348048f14bd8af3fd9
z3r0-lab <abagjan05@gmail.com>
2022-08-24 22:06:18 +0600

    Add images and other file

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
 
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         #Назначение цвета фона.
         self.bg_color = (230, 230, 230)
@@ -18,16 +25,21 @@ class AlienInvasion:
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
-            #Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type() == pygame.QUIT:
+            self._check_events()
+            self._update_screen()
+    
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши."""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     sys.exit()
             
-            #При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.bg_color)
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            #Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Создание экземпляра и запуск игры.
